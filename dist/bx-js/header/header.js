cb0c801a8b63d1eb6c628fda74c8b6f7441d93dd
Бескишкова Анастасия <61845053+Staasik@users.noreply.github.com>
2023-10-20 18:13:15 +0300

    fix

--- dist/bx-js/header/header.js
+++ dist/bx-js/header/header.js
@@ -5,21 +5,22 @@ $(function () {
     var screenWidth = window.innerWidth;
     var logo_menu = document.getElementById('menu_hamburger');
     var menu = document.getElementById('menu');
-    var logo = document.getElementById('logo');
+    ъ;
     var header__right = document.getElementById('header__right-block');
     var header__left = document.getElementById('header__left-block');
+    var header_logo = document.getElementById('header_logo');
     if (screenWidth <= 1200) {
-      logo.style.display = 'block';
       logo_menu.style.display = 'block';
       menu.style.display = 'none';
       header__right.style.display = 'none';
       header__left.style.display = 'none';
+      header_logo.style.display = 'block';
     } else {
       logo_menu.style.display = 'none';
-      logo.style.display = 'none';
       menu.style.display = 'flex';
       header__right.style.display = 'flex';
       header__left.style.display = 'flex';
+      header_logo.style.display = 'none';
     }
   }
   window.addEventListener('load', hideElements);
