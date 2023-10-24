8b4c66b9584a4030526a1313ec17eed77923212f
Бескишкова Анастасия <61845053+Staasik@users.noreply.github.com>
2023-10-24 14:27:27 +0300

    fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -155,20 +155,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function($) {$(function () {
-  // Функция для изменения изображения в зависимости от ширины экрана
-  function updateImage() {
-    var bannerImage = document.getElementById('bannerImage');
-    var screenWidth = window.innerWidth;
-    if (screenWidth <= 1000) {
-      bannerImage.src = './img/content/banner_768х583.png';
-    } else {
-      bannerImage.src = './img/content/banner_1920х800.png';
-    }
-  }
-  window.addEventListener('load', updateImage);
-  window.addEventListener('resize', updateImage);
-});
+/* WEBPACK VAR INJECTION */(function($) {$(function () {});
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "jquery")))
 
 /***/ }),
--- src/blocks/modules/banner/banner.js
+++ src/blocks/modules/banner/banner.js
@@ -1,14 +1,3 @@
 $(function () {
-  // Функция для изменения изображения в зависимости от ширины экрана
-  function updateImage() {
-    const bannerImage = document.getElementById('bannerImage');
-    const screenWidth = window.innerWidth;
-    if (screenWidth <= 1000) {
-      bannerImage.src = './img/content/banner_768х583.png';
-    } else {
-      bannerImage.src = './img/content/banner_1920х800.png';
-    }
-  }
-  window.addEventListener('load', updateImage);
-  window.addEventListener('resize', updateImage);
+
 })
